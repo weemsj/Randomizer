17ee34f9c37dcff363e19e8197ed58b652e390d8
weemsj <71594052+weemsj@users.noreply.github.com>
2022-05-04 10:49:38 +0200

    added some test cases

--- randomizer.py
+++ randomizer.py
@@ -1,59 +1,71 @@
-import http.server
+
 import random
 import time
 
-hostName = "localhost"
-serverPort = 8080
 
 def randomizer():
     
-    while True:
-        time.sleep(3)
-        f = open('pipeline.txt', 'r')
+    time.sleep(3)
+    
+    f = open('pipeline.txt', 'r')
 
-        contents = f.readline()
+    contents = f.read()
 
-        # checks for a single number
-        if contents.isdigit():
-            num = int(contents)
-            rand = random.randrange(num)
+    if not contents:
+        return 
+    
+    # checks for a single number
+    if contents.isdigit():
+        num = int(contents)
+        rand = random.randrange(num)
 
-        else:
-            isNum = 0
-            # puts contents into a list of strings
-            lis = contents.split(',')
+    else:
+        isNum = 0
+        # puts contents into a list of strings
+        lis = contents.split(',')
 
-            # checks for a list of 3 strings or less indicating a possible string of numbers
-            # if len(lis) <=3 then check each if each string is a number, if all string are numbers randomize by range.
-            if len(lis) <= 3:
-                for i in lis:
-                    if i.isdigit():
-                        isNum = True
-                    else:
-                        isNum = False
-
-            if isNum == True:  # indicates a list of numbers
-                for i in range(len(lis)):
-                    lis[i] = int(lis[i])
-        
-                if len(lis) == 3:
-                    rand = random.randrange(lis[0], lis[1], lis[2])
-                elif len(lis) == 2:
-                    rand = random.randrange(lis[0], lis[1])
+        # checks for a list of 3 strings or less indicating a possible string of numbers
+        # if len(lis) <=3 then check each if each string is a number, if all string are numbers randomize by range.
+        if len(lis) <= 3:
+            for i in lis:
+                if i.isdigit():
+                    isNum = True
                 else:
-                    rand = random.randrange(lis[0])
+                    isNum = False
 
+        if isNum == True:  # indicates a list of numbers
+            for i in range(len(lis)):
+                lis[i] = int(lis[i])
+        
+            if len(lis) == 3 and lis[0] < lis[1] > lis[2]:
+                rand = random.randrange(lis[0], lis[1], lis[2])
+            elif len(lis) == 2 and lis[0]< lis[1]:
+                rand = random.randrange(lis[0], lis[1])
+            elif len(lis) == 1:
+                rand = random.randrange(lis[0])
             else:
-                num = len(lis)
-                if num <= 1:
-                    return 
-                randNum = random.randrange(num)
-                rand = lis[randNum]
+                with open('pipeline.txt', 'w') as f:
+                    f.write("Error, invalid integer, randomizer expects (start,stop,step). Start and stop are optional. ")
+                    f.close()
+                    return
+        else:
+            num = len(lis)
+            if num <= 1:
+                return 
+            randNum = random.randrange(num)
+            rand = lis[randNum]
 
-        with open('pipeline.txt', 'w') as f:
-                f.write(rand)
+    with open('pipeline.txt', 'w') as f:
+        if type(rand) == int:
+            rand = str(rand)
+            print(type(rand))
+        f.write(rand)
+        f.close()
+        return
+        
     
             
 
 if __name__ == "__main__":
-   randomizer()
+   while True:
+        randomizer()
---
+++ test.py
@@ -0,0 +1,76 @@
+import time
+
+
+def write(lis):
+    with open('pipeline.txt', 'w') as f:
+        for i in lis: 
+            if i != lis[-1]:  # if the list item is the only item or is the last item dont include a comma at the end.
+                if type(i) == int: # check for an integer write function only works with strings.
+                    i = str(i)
+                f.write(i + ",")
+            else:
+                if type(i) == int:
+                    i = str(i)
+                f.write(i)
+    f.close()
+
+
+def read():
+    f = open('pipeline.txt', 'r')
+    contents = f.readline()
+    f.close()
+    print(contents)
+    
+
+if __name__ == '__main__':
+    
+    """ Below are some example calls of what data the randomizer expects to read from the pipeline.txt file."""
+    write([15]) # user passes a single int (stop)
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write([10,30]) # user passes two ints (start, stop)
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write([10,30, 2]) # user passes three ints (start, stop, step)
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write(['little']) # user one string
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write(['small', 'big', 'medium', 'large', 'gigantic']) # user passes multiple strings
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write(['10 Smallest Pigeons', 'The Top 50 Most Dangerous Animals', '100 Shocking Facts' ]) # user passes strings that contain numbers
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write(['']) # if user passes emplty string no crashes occur but also no errors are returned
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    write([30,10]) # if user passes a larger start than stop, the randomizer will return an error message
+    time.sleep(3.5)
+    read()
+    lis = []
+    write(lis)
+    
+    
